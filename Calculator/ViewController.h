80f2fdab2f8a23c2792cdb34851b23f10abfffa3
shawnchendev <cola9889@gmail.com>
2017-01-24 15:41:21 -0330

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Shawn Chen on 2017-01-24.
+//  Copyright © 2017 Shawn Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Shawn Chen on 2017-01-24.
+//  Copyright © 2017 Shawn Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
